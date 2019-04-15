e3bb5bc199ecaef71d54636e7c98a89a76679f18
kobayashi <kobayashi.wataru@donika.co.jp>
2019-04-15 16:43:44 +0900

    それとなくUI周りを調整

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
 
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$apiaxios = axios.create({
+  baseURL: 'https://ika.queloud.net',
+  headers: {
+    Authorization: `Bearer ${process.env.VUE_APP_ACCESS_TOKEN}`
+  }
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="home container text-center">
+  <div class="home text-center">
     <HelloWorld msg="イカトドン肉派サイトのために色々するサイト"/>
 
-    <div class="mb-5">
-      <button class="btn btn-primary" @click="fetchLists">リストを取得する</button>
-    </div>
-
-    <section>
-      <h3>肉派メンバー（抜粋）</h3>
-      <div class="row">
-        <div class="col-4 col-md-2" v-for="(list, index) in lists" :key="index">
-          <a :href="list.url">
-            <img class="img-fluid rounded-circle w-100" :src="list.avatar" :alt="list.display_name">
-            <span v-html="list.display_name"></span>
-          </a>
+    <section class="l-members">
+      <div class="container">
+        <h3 class="heading">イカしたニクショクチーム</h3>
+        <div class="row" v-if="lists.length > 0">
+          <div class="l-members-box-wrapper col-6 col-md-3 mb-4" v-for="(list, index) in lists" :key="index">
+            <div class="l-members-box p-4 rounded">
+              <div class="l-members-box-icon text-center">
+                <img class="rounded-circle" :src="list.avatar" :alt="list.display_name">
+              </div>
+              <div class="display-name mb-2">
+                {{list.display_name}}
+              </div>
+              <div>
+                <div class="username text-truncate mb-2">
+                  <a :href="list.url">@{{list.username}}</a>
+                </div>
+                <button class="btn btn-outline-dark btn-more">more</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -22,16 +30,8 @@
 
 <script>
 // @ is an alias to /src
-import axios from 'axios'
+const _ = require("lodash");
 import HelloWorld from '@/components/HelloWorld.vue'
-
-// ちゃんとやるならthis.$axiosとかで使えるようにしなきゃね〜
-const apiaxios = axios.create({
-  baseURL: 'https://ika.queloud.net',
-  headers: {
-    Authorization: `Bearer ${process.env.VUE_APP_ACCESS_TOKEN}`
-  }
-})
 
 export default {
   name: 'home',
@@ -43,11 +43,81 @@ export default {
       lists: [],
     }
   },
+  created () {
+    this.fetchLists()
+  },
   methods: {
     async fetchLists() {
-      const res = await apiaxios.get(`/api/v1/lists/${process.env.VUE_APP_LISTID}/accounts`)
-      this.lists = res.data
+      const res = await this.$apiaxios.get(`/api/v1/lists/${process.env.VUE_APP_LISTID}/accounts`)
+      this.lists = _.orderBy(res.data, 'username', 'asc')
     }
   }
 }
 </script>
+
+<style lang="scss">
+.heading {
+  font-family: 'Noto Sans CJK JP';
+  text-align: center;
+  padding: 5rem 0
+}
+
+.l-members {
+  background-color: #FFCBCB;
+  font-weight: bold;
+  font-size: 20px;
+
+  .l-members-box {
+    background-color: #FF8383;    
+    .l-members-box-icon {
+      img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+  }
+
+  .display-name {
+    height: 90px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  .username {
+    a {
+      color: #4C3535;
+    }
+  }
+
+  .btn-more {
+    position: relative;
+
+    &.btn-outline-dark {
+      border-radius: 5rem;
+      padding: .25rem 2rem;
+
+      border: 4px solid;
+      font-size: 20px;
+      font-weight: 900;
+
+      color: #4C3535;
+      border-color: #4C3535;
+      &:hover,
+      &:active {
+        color: #FF8383 !important;
+      }
+    }
+    &::after {
+      position: absolute;
+      right: 0;
+      margin-top: .05rem;
+      margin-right: .5rem;
+      font-family: "Font Awesome 5 Free";
+      content: "\f105";
+    }
+  }
+}
+</style>
